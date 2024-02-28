5bb0bf7302b0347cd50562b6386bd7e166e62f79
Toby Liu <ybot1122@gmail.com>
2024-02-28 15:25:09 -0800

    Recipe instructions

--- src/app/recipes/[slug]/page.tsx
+++ src/app/recipes/[slug]/page.tsx
@@ -46,6 +46,9 @@ export default async function Project(params: Params) {
     images,
     prepTime,
     totalTime,
+    totalYield,
+    recipeIngredients,
+    recipeInstructions,
   } = await getData();
 
   return (
@@ -60,7 +63,7 @@ export default async function Project(params: Params) {
           />
         </div>
         <div className="grid grid-cols-2">
-          <div className="bg-blue">
+          <div className="bg-white">
             <h1 className="font-primary text-2xl font-bold md:text-4xl mb-2">
               {recipeName}
             </h1>
@@ -69,6 +72,8 @@ export default async function Project(params: Params) {
               Published on <DateFormatter dateString={publishedAt} />
             </div>
             <div>{description}</div>
+
+            {/* Metadata */}
             <div className="grid grid-cols-2">
               <div className="col-span-1">
                 <p>Prep Time</p>
@@ -78,9 +83,12 @@ export default async function Project(params: Params) {
                 <p>Total Time</p>
                 <p>{totalTime}</p>
               </div>
+              <div className="border-t-4">
+                <p>Yield</p>
+                <p>{totalYield}</p>
+              </div>
             </div>
           </div>
-
           <div className="col-span-1">
             {images.gallery.map((img) => (
               <Image
@@ -91,6 +99,41 @@ export default async function Project(params: Params) {
                 key={img}
               />
             ))}
+          </div>
+        </div>
+
+        {/* Ingredients and Instructions */}
+        <div className="grid grid-cols-4 bg-tan p-20">
+          <div className="col-span-1">
+            <h2 className="text-xl mb-5 mt-10">Ingredients</h2>
+            <ul>
+              {recipeIngredients.map((ingredient, ind) => (
+                <li key={ind} className="mb-5 mr-5">
+                  {ingredient}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-span-3 bg-white p-10">
+            <h2 className="text-xl mb-5">Instructions</h2>
+            <ol className="list-decimal">
+              {recipeInstructions.map((step, ind) => (
+                <>
+                  <li key={ind} className="">
+                    {step.text}
+                  </li>
+                  {step.image && (
+                    <Image
+                      src={step.image}
+                      alt={`${recipeName} step ${ind + 1}`}
+                      width={50}
+                      height={50}
+                      key={`img-${ind}`}
+                    />
+                  )}
+                </>
+              ))}
+            </ol>
           </div>
         </div>
       </article>
@@ -112,7 +155,8 @@ async function getData() {
     datePublished: "December 31, 2009 at 7:00pm",
     prepTime: "42 mins",
     totalTime: "12 hrs 42 mins",
-    recipeIngredient: [
+    totalYield: 'about 2 dozen medium pancakes or 1 dozen 8" waffles',
+    recipeIngredients: [
       "2 cups (240g) King Arthur Unbleached All-Purpose Flour",
       "1 cup (227g) sourdough starter unfed/discard",
       "2 tablespoons (28g) granulated sugar",
@@ -122,6 +166,44 @@ async function getData() {
       "1/4 cup (50g) vegetable oil or 4 tablespoons (57g) butter melted",
       "3/4 teaspoon table salt",
       "1 teaspoon baking soda",
+    ],
+    recipeInstructions: [
+      {
+        text: "To make the overnight sponge: Stir down your refrigerated starter, and remove 1 cup (227g). Note: This is a good opportunity to feed the remainder, if necessary.",
+        image: null,
+      },
+      {
+        text: "In a large bowl, stir together the 1 cup (227g) unfed starter, flour, sugar, and buttermilk.",
+        image: "/images/couple-pizza.png",
+      },
+      {
+        text: "Cover and let rest at cool room temperature (about 65°F to 70°F) for about 12 hours, or overnight.",
+        image: "/images/flycream-travel.png",
+      },
+      {
+        text: "To make the batter: In a small bowl or mixing cup, beat together the eggs, and oil or butter. Add to the overnight sponge, stirring just to combine.",
+        image: null,
+      },
+      {
+        text: "Add the salt and baking soda, stirring to combine. The batter will expand and may bubble a bit.",
+        image: "/images/flycream-travel.png",
+      },
+      {
+        text: "To make pancakes: Pour the batter by the 1/4-cupful onto a preheated, lightly greased griddle. Cook until bubbles form and pop on the top side of the pancakes, then turn over and cook until browned underneath.",
+        image: "/images/flycream-travel.png",
+      },
+      {
+        text: "To make waffles: Pour the batter onto your preheated, greased waffle iron, and bake according to the manufacturer's instructions. Repeat with the remaining batter.",
+        image: "/images/flycream-travel.png",
+      },
+      {
+        text: "Serve pancakes or waffles immediately, with your favorite toppings; or hold in a warm oven until ready to serve.",
+        image: null,
+      },
+      {
+        text: "Storage instructions: Store any leftovers in the refrigerator for a day or two; freeze for longer storage.",
+        image: null,
+      },
     ],
     images: {
       hero: "/images/industrial-pattern.png",
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,8 @@ module.exports = {
     colors: {
       transparent: "transparent",
       current: "currentColor",
-      white: "#ffffff",
-      purple: "#3f3cbb",
-      midnight: "#121063",
-      metal: "#565584",
-      tahiti: "#3ab7bf",
-      silver: "#ecebff",
-      "bubble-gum": "#ff77e9",
-      bermuda: "#78dcca",
+      white: "#FFFFFF",
+      tan: "rgb(248, 245, 240)",
     },
   },
 };
