c61a062f67d61f1dec3d21c3f49aebd3c82ebcbb
Toby Liu <ybot1122@gmail.com>
2024-03-02 19:08:59 -0800

    Image upload option

--- src/components/ImageChooser.tsx
+++ src/components/ImageChooser.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useState } from "react";
 
 const ImageChooser = ({
   images,
@@ -9,33 +10,97 @@ const ImageChooser = ({
   close: () => void;
   selectImage: (src: string) => void;
 }) => {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [activeTab, setActiveTab] = useState<"choose" | "upload">("choose");
+
   return (
     <div className="fixed top-0 left-0 w-full h-full">
       <div className="absolute top-0 left-0 bottom-0 right-0 w-[600px] h-[600px] bg-silver mx-auto my-auto">
         <button onClick={close} className="absolute right-5 p-5">
           X
         </button>
-        <h2 className="text-center mt-5 text-2xl">Choose an Image</h2>
-        <div className="overflow-y-auto h-[500px]">
-          <div className="grid grid-cols-4 gap-2">
-            {images.map((src, ind) => (
-              <div
-                key={ind}
-                className="p-2 border-2 border-silver hover:border-orange"
-              >
-                <button onClick={() => selectImage(src)}>
-                  <Image
-                    src={`/images/${src}`}
-                    alt="image chooser"
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    style={{ width: "100%", height: "100%" }} // optional
-                  />
-                </button>
-              </div>
-            ))}
+        <div className="grid grid-cols-2 gap-2">
+          <div>
+            <h2
+              className="text-center mt-5 text-2xl cursor-pointer hover:underline"
+              onClick={() => setActiveTab("choose")}
+            >
+              Choose an Image
+            </h2>
           </div>
+          <div>
+            <h2
+              className="text-center mt-5 text-2xl cursor-pointer hover:underline"
+              onClick={() => setActiveTab("upload")}
+            >
+              Upload Image
+            </h2>
+          </div>
+        </div>
+
+        <div className="overflow-y-auto h-[500px]">
+          {activeTab === "choose" && (
+            <div className="grid grid-cols-4 gap-2">
+              {images.map((src, ind) => (
+                <div
+                  key={ind}
+                  className="p-2 border-2 border-silver hover:border-orange"
+                >
+                  <button onClick={() => selectImage(src)}>
+                    <Image
+                      src={`/images/${src}`}
+                      alt="image chooser"
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      style={{ width: "100%", height: "100%" }} // optional
+                    />
+                  </button>
+                </div>
+              ))}
+            </div>
+          )}
+
+          {activeTab === "upload" && (
+            <div className="text-center mt-10">
+              <input
+                type="file"
+                name="myImage"
+                className="block mx-auto mb-10"
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  console.log(event?.target?.files?.[0]);
+                  const file = event?.target?.files?.[0];
+
+                  if (file) {
+                    setSelectedImage(file);
+                  }
+                }}
+              />
+              {selectedImage && (
+                <div className="inline-block">
+                  <img
+                    alt="not found"
+                    width={"250px"}
+                    src={URL.createObjectURL(selectedImage)}
+                  />
+                  <br />
+                  <button
+                    className="inline-block border-2 border-orange mr-5 p-2"
+                    onClick={() => setSelectedImage(null)}
+                  >
+                    Remove
+                  </button>
+
+                  <button
+                    className="inline-block border-2 border-orange p-2"
+                    onClick={() => setSelectedImage(null)}
+                  >
+                    Upload
+                  </button>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </div>
