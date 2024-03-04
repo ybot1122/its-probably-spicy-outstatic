fa05ac9f278b7a86732ee13c8fa006c2eff16cd0
Toby Liu <toby.liu@hulu.com>
2024-03-04 10:47:38 -0800

    Trying to use cloudinary>

--- src/app/api/admin/imageUploadv2/route.ts
+++ src/app/api/admin/imageUploadv2/route.ts
@@ -1,1 +1,43 @@
 // Using Cloudinary
+
+// TODO?
+import { UploadApiResponse, v2 as cloudinary } from "cloudinary";
+
+export async function POST(request: Request) {
+  const formData = await request.formData();
+  const image = formData.get("image") as File;
+  const content = formData.get("raw") as string;
+  const filename = image.name;
+
+  if (!image || !content)
+    return new Response("Invalid request for image upload", {
+      status: 400,
+    });
+
+  cloudinary.config({
+    cloud_name: "dryy6uo6k",
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET,
+  });
+
+  const startUpload = () =>
+    new Promise<UploadApiResponse>((resolve, reject) => {
+      cloudinary.uploader.upload(
+        content,
+        { public_id: filename },
+        function (error, result) {
+          console.log(error, result);
+          if (error || !result) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
+        },
+      );
+    });
+
+  const res = await startUpload();
+  console.log(res);
+
+  return new Response(res.public_id + "." + res.format, { status: 200 });
+}
