f020d428cb538562525b9111ea489963e8a8c150
Toby Liu <toby.liu@hulu.com>
2024-03-04 12:12:23 -0800

    Try to set cookie?

--- src/app/(newcms)/admin/login/page.tsx
+++ src/app/(newcms)/admin/login/page.tsx
@@ -1,28 +1,23 @@
 import Link from "next/link";
-
-export const AdminButton = ({
-  text,
-  type,
-}: {
-  text: string;
-  type?: "submit";
-}) => (
-  <button
-    type={type}
-    className="border-silver border-2 hover:border-orange rounded p-10 transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300 "
-  >
-    {text}
-  </button>
-);
+import { AdminButton } from "@/app/(newcms)/admin/page";
+import { useCallback } from "react";
+import { loginUserAction } from "@/app/actions";
 
 export default async function Page() {
+  async function loginUser() {
+    "use server";
+    loginUserAction("fake code");
+  }
+
   return (
     <div className="m-20 max-w-lg mx-auto">
       <h1 className="text-6xl text-center">Welcome to the Admin Panel</h1>
 
       <div className="grid grid-cols-2 text-center mt-20">
         <div className="col-span-2">
-          <AdminButton text="Click to Login" />
+          <form action={loginUser}>
+            <AdminButton text="Click to Login" type="submit" />
+          </form>
         </div>
       </div>
     </div>
