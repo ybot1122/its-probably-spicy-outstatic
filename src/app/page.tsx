2d88dbc7af499d3ff3f34a7b3fff3df94ffde789
Toby Liu <ybot1122@gmail.com>
2024-03-07 16:32:00 -0800

    Some images?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,16 +14,13 @@ export default async function Index() {
   return (
     <Layout>
       <div className="max-w-screen-lg mx-auto px-5 mt-10">
-        <section
-          className={`${kalam.className} text-3xl md:text-6xl font-light`}
-        >
-          <div className="relative float-right w-[128px] h-[128px]">
+        <section className={`${kalam.className} text-3xl font-light`}>
+          <div className="relative float-right w-[128px] h-[128px] m-2">
             <Image
               src="/images/daisy.jpeg"
               fill
-              objectFit="cover"
               alt="Head Chef Daisy"
-              className="rounded-full"
+              className="rounded-full object-cover"
             />
           </div>
           <p>
@@ -31,11 +28,47 @@ export default async function Index() {
             <span className="text-orange font-semibold">flavors</span>, hope you
             get to try them all!
           </p>
-          <span
-            className={`block ${libre_baskerville.className} text-3xl mt-10`}
-          >
-            ~ Head Chef Daisy
-          </span>
+          <span className={`block mt-10`}>~ Head Chef Daisy</span>
+        </section>
+
+        <section
+          className={`mt-20 ${libre_baskerville.className} grid grid-cols-4 gap-2`}
+        >
+          <div className="relative w-full h-[250px] col-span-2 md:col-span-1">
+            <Image
+              src="/images/IMG_3320.jpg"
+              fill
+              alt="Head Chef Daisy"
+              className="rounded-lg object-cover"
+            />
+          </div>
+
+          <div className="relative w-full h-[250px] col-span-2 md:col-span-1">
+            <Image
+              src="/images/IMG_3438.jpg"
+              fill
+              alt="Head Chef Daisy"
+              className="rounded-lg object-cover"
+            />
+          </div>
+
+          <div className="relative w-full h-[250px] col-span-2 md:col-span-1">
+            <Image
+              src="/images/IMG_3429.jpg"
+              fill
+              alt="Head Chef Daisy"
+              className="rounded-lg object-cover"
+            />
+          </div>
+
+          <div className="relative w-full h-[250px] col-span-2 md:col-span-1">
+            <Image
+              src="/images/IMG_3440.jpg"
+              fill
+              alt="Head Chef Daisy"
+              className="rounded-lg object-cover"
+            />
+          </div>
         </section>
 
         <div className="mt-20">
