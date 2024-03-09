d5edce7c5fd31c7a72aa19116ff16cc435581626
Toby Liu <ybot1122@gmail.com>
2024-03-09 15:48:03 -0800

    Some data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,24 @@ import Image from "next/image";
 import { FullHero } from "@/components/FullHero";
 import { libre_baskerville, kalam } from "@/app/fonts";
 
+const TEMP = [
+  "IMG_3320.jpg",
+  "IMG_3438.jpg",
+  "IMG_3429.jpg",
+  "IMG_3440.jpg",
+  "IMG_3320.jpg",
+  "IMG_3438.jpg",
+  "IMG_3429.jpg",
+  "IMG_3440.jpg",
+];
+
 export default async function Index() {
   const { allRecipes } = await getData();
 
   return (
     <Layout>
-      <div className="max-w-screen-lg mx-auto px-5 mt-10">
-        <section>
+      <div className="w-full mt-10">
+        <section className="max-w-screen-lg mx-auto px-5">
           <h1
             className={`max-w-screen-sm mx-auto text-xl md:text-3xl ${kalam.className} text-center py-5`}
           >
@@ -34,60 +45,40 @@ export default async function Index() {
             />{" "}
           </div>
         </section>
-        <section className={`${kalam.className} text-3xl font-light mt-20`}>
-          <div className="relative float-right w-[128px] h-[128px] m-2">
-            <Image
-              src="/images/daisy.jpeg"
-              fill
-              alt="Head Chef Daisy"
-              className="rounded-full object-cover"
-            />
-          </div>
-          <p>
-            I hope you enjoy my delicious recipes. We have so many different{" "}
-            <span className="text-orange font-semibold">flavors</span>, hope you
-            get to try them all!
-          </p>
-          <span className={`block mt-10`}>~ Head Chef Daisy</span>
-        </section>
 
-        <section
-          className={`mt-20 ${libre_baskerville.className} grid grid-cols-4 gap-2`}
-        >
-          <div className="relative w-full h-[250px] col-span-2 md:col-span-1">
-            <Image
-              src="/images/IMG_3320.jpg"
-              fill
-              alt="Head Chef Daisy"
-              className="rounded-lg object-cover"
-            />
+        {/*image zoom https://w3bits.com/labs/css-image-hover-zoom/ */}
+        <section className={`${libre_baskerville.className} `}>
+          <div className="flex max-w-screen-lg mx-auto items-center mt-10">
+            <h1
+              className={` text-xl md:text-3xl ${kalam.className} text-left p-5`}
+            >
+              MORE RECIPES
+            </h1>
+
+            <span
+              className="inline-block grow h-[2px] bg-orange"
+              aria-hidden="true"
+            ></span>
           </div>
 
-          <div className="relative w-full h-[250px] col-span-2 md:col-span-1">
-            <Image
-              src="/images/IMG_3438.jpg"
-              fill
-              alt="Head Chef Daisy"
-              className="rounded-lg object-cover"
-            />
-          </div>
-
-          <div className="relative w-full h-[250px] col-span-2 md:col-span-1">
-            <Image
-              src="/images/IMG_3429.jpg"
-              fill
-              alt="Head Chef Daisy"
-              className="rounded-lg object-cover"
-            />
-          </div>
-
-          <div className="relative w-full h-[250px] col-span-2 md:col-span-1">
-            <Image
-              src="/images/IMG_3440.jpg"
-              fill
-              alt="Head Chef Daisy"
-              className="rounded-lg object-cover"
-            />
+          <div className="max-w-screen-lg mx-auto pb-20 px-5 grid grid-cols-4 gap-4">
+            {TEMP.map((t) => (
+              <div className="col-span-2 md:col-span-1 mb-10 bg-tan" key={t}>
+                <div className="w-full aspect-square border-orange p-2">
+                  <div className="relative w-full aspect-square">
+                    <Image
+                      src={`/images/${t}`}
+                      fill
+                      alt={t}
+                      className="object-cover"
+                    />
+                  </div>
+                </div>
+                <div className="grid place-items-center pb-2">
+                  <h2>Recipe Title Here</h2>
+                </div>
+              </div>
+            ))}
           </div>
         </section>
 
@@ -110,3 +101,26 @@ async function getData() {
     allRecipes,
   };
 }
+
+/*
+
+        <section className={`${kalam.className} text-3xl font-light mt-20`}>
+          <div className="relative float-right w-[128px] h-[128px] m-2">
+            <Image
+              src="/images/daisy.jpeg"
+              fill
+              alt="Head Chef Daisy"
+              className="rounded-full object-cover"
+            />
+          </div>
+          <p>
+            I hope you enjoy my delicious recipes. We have so many different{" "}
+            <span className="text-orange font-semibold">flavors</span>, hope you
+            get to try them all!
+          </p>
+          <span className={`block mt-10`}>~ Head Chef Daisy</span>
+        </section>
+
+
+
+        */
