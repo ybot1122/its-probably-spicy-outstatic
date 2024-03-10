2c5e5080e19bd36a1118d829a48bb4066718d71c
Toby Liu <ybot1122@gmail.com>
2024-03-09 20:42:19 -0800

    Fix image gallery

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -19,31 +19,27 @@ const ImageGallery = ({ images }: ImageGalleryProps) => {
     <Image
       src={images[activeImageInd].src}
       alt={images[activeImageInd].alt}
-      width={0}
-      height={0}
-      sizes="100vw"
-      style={{ width: "100%", height: "100%" }} // optional
+      fill
+      className="object-cover"
     />
   );
 
   return (
     <div className="relative w-full h-full">
-      <div>{activeImage}</div>
+      <div className="relative aspect-video">{activeImage}</div>
       <div className="grid grid-cols-3 mt-5">
         {images.length > 1 &&
           images.map((img, ind) => (
             <div
-              className={`col-span-1 ${ind < 2 && "mr-5"} ${ind === activeImageInd ? "border-orange border-2" : ""} cursor-pointer`}
+              className={`relative aspect-video col-span-1 ${ind < 2 && "mr-5"} ${ind === activeImageInd ? "border-orange border-2" : ""} cursor-pointer`}
               key={`img-gallery-${ind}`}
               onClick={() => setActiveImageInd(ind)}
             >
               <Image
                 src={img.src}
                 alt={img.alt}
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{ width: "100%", height: "100%" }} // optional
+                fill
+                className="object-cover"
               />
             </div>
           ))}
