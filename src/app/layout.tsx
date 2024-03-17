659609b918738c28bbdbbabc7d9ebdc68a7a314d
Toby Liu <ybot1122@gmail.com>
2024-03-16 21:05:06 -0700

    Adding some metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,24 +4,63 @@ import { mooli } from "./fonts";
 import "../styles/index.css";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://its-probably-spicy-outstatic.vercel.app"),
   title: {
-    default: "It's Probably Spicy",
     template: "%s | It's Probably Spicy",
+    default: "It's Probably Spicy",
   },
-  description: "A Recipe Blog",
+  description:
+    "Head Chef Daisy presents her amazing recipes from all around the world. Just remember, It's Probably Spicy!",
+  generator: "Next.js",
+  applicationName: "It's Probably Spicy",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "Its Probably Spicy",
+    "Its",
+    "Probably",
+    "Spicy",
+    "Recipe",
+    "Recipes",
+    "Blog",
+    "Recipe Blog",
+    "Recipes Blog",
+    "Nigerian",
+    "Nigerian Food",
+    "Nigerian Recipes",
+    "Nigerian Food Recipes",
+  ],
+  authors: [{ name: "Daisy" }],
+  creator: "Toby",
+  publisher: "It's Probably Spicy",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
   openGraph: {
     title: "It's Probably Spicy",
-    description: "A recipe blog.",
+    description:
+      "Head Chef Daisy presents her amazing recipes from all around the world. Just remember, It's Probably Spicy!",
     url: absoluteUrl("/"),
     siteName: "It's Probably Spicy",
-    images: [],
     locale: "en_US",
     type: "website",
+    /*
+    publishedTime: '2023-01-01T00:00:00.000Z',
+    authors: ['Seb', 'Josh'],
+    */
   },
-  icons: {
-    icon: [{ url: "/favicon/favicon-32x32.png" }],
-    apple: [{ url: "/favicon/apple-touch-icon.png" }],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
   },
 };
 
